25ad4500baaef40aa08a9f3b72ce7bd64d0d6fc2
zagob <matheusbestana@gmail.com>
2025-03-24 18:10:48 -0300

    implementing form register transaction

--- src/components/RegisterTransactionDialog.tsx
+++ src/components/RegisterTransactionDialog.tsx
@@ -9,12 +9,8 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
-import { CalendarIcon } from "lucide-react";
 import { Calendar } from "./ui/calendar";
 import { useState } from "react";
-import { cn } from "@/lib/utils";
-import { format } from "date-fns";
 import {
   Select,
   SelectContent,
@@ -23,15 +19,40 @@ import {
   SelectValue,
 } from "./ui/select";
 import { Input } from "./ui/input";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Textarea } from "./ui/textarea";
 
 interface RegisterTransactionDialogProps {
   type: "income" | "expense";
 }
 
+const formSchema = z.object({
+  date: z.date(),
+  bank: z.string(),
+  categories: z.string(),
+  description: z.string(),
+  value: z.string(),
+});
+
+type FormSchemaProps = z.infer<typeof formSchema>;
+
 export const RegisterTransactionDialog = ({
   type,
 }: RegisterTransactionDialogProps) => {
+  const form = useForm<FormSchemaProps>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      date: new Date(),
+    },
+  });
   const [date, setDate] = useState<Date>();
+
+  const handleSubmitForm = (values: FormSchemaProps) => {
+    console.log(values);
+  };
 
   return (
     <Dialog>
@@ -55,55 +76,121 @@ export const RegisterTransactionDialog = ({
       <DialogContent className="bg-zinc-800 border-zinc-700">
         <DialogHeader>
           <DialogTitle>Add {type}</DialogTitle>
-          <DialogDescription>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant={"outline"}
-                  className={cn(
-                    "w-[280px] justify-start text-left font-normal",
-                    !date && "text-muted-foreground"
+          <DialogDescription asChild>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(handleSubmitForm)}
+                className="flex flex-col gap-2"
+              >
+               <div className="flex gap-2">
+               <FormField
+                  control={form.control}
+                  name="date"
+                  render={({ field }) => (
+                    <Calendar
+                      mode="single"
+                      disableNavigation
+                      selected={date}
+                      onSelect={setDate}
+                      className="rounded-md mx-auto"
+                      {...field}
+                    />
                   )}
-                >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0">
-                <Calendar
-                  mode="single"
-                  selected={date}
-                  onSelect={setDate}
-                  initialFocus
                 />
-              </PopoverContent>
-            </Popover>
 
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
+                <div className="flex flex-col gap-2">
+                  <FormField
+                    control={form.control}
+                    name="bank"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Bank:</FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="dark:text-neutral-200 w-full dark:hover:bg-neutral-900 dark:bg-neutral-900 dark:border-neutral-700">
+                              <SelectValue placeholder="Select a bank" />
+                            </SelectTrigger>
+                            <SelectContent className="dark:bg-neutral-400 border-none">
+                              <SelectItem value="bank1">Bank1</SelectItem>
+                              <SelectItem value="bank2">Bank2</SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
 
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
+                  <FormField
+                    control={form.control}
+                    name="categories"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Categories:</FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="w-full dark:text-neutral-200 dark:hover:bg-neutral-900 dark:bg-neutral-900 dark:border-neutral-700">
+                              <SelectValue placeholder="Select a category" />
+                            </SelectTrigger>
+                            <SelectContent className="dark:bg-neutral-400 border-none">
+                              <SelectItem value="categorie1">
+                                categorie1
+                              </SelectItem>
+                              <SelectItem value="categorie2">
+                                categorie2
+                              </SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                
 
-            <Input placeholder="value" />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description:</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          className="resize-none dark:bg-neutral-900 dark:border-neutral-700"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
 
-            <Button>Register Transaction</Button>
+                <FormField
+                  control={form.control}
+                  name="value"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Value:</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="dark:bg-neutral-900 dark:border-neutral-700"
+                          placeholder="10"
+                          {...field}
+                        />
+                      </FormControl>
+                      {/* <span>R$</span> */}
+                    </FormItem>
+                  )}
+                />
+                </div>
+               </div>
+
+                <Button type="submit">Register Transaction</Button>
+              </form>
+            </Form>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
