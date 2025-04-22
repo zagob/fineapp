4bd70eed65158421cfb4153012546cc3cba53718
zagob <matheusbestana@gmail.com>
2025-04-22 18:15:27 -0300

    add recharts page transactions

--- src/app/(root)/transactions/page.tsx
+++ src/app/(root)/transactions/page.tsx
@@ -26,6 +26,8 @@ import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { X } from "lucide-react";
+import { RechartIncome } from "./RechartIncome";
+import { RechartExpense } from "./RechartExpense";
 
 export default function Page() {
   const date = useDateStore((state) => state.date);
@@ -38,24 +40,24 @@ export default function Page() {
     setBank,
   } = useTransactionStore();
 
-  const { data: transactions, isPending } = useQuery({
+  const { data, isPending } = useQuery({
     queryKey: ["transactions", date, typeTransaction, category, bank],
     queryFn: async () => {
-      const { transactionsByDate } = await getTransactions({
+      const { transactionsByDate, resume } = await getTransactions({
         date,
         type: typeTransaction ?? undefined,
         categoryId: category.length > 0 ? category : undefined,
         bankId: !bank || bank.length === 0 ? undefined : bank,
       });
 
-      return transactionsByDate;
+      return { transactionsByDate, resume };
     },
   });
 
-  const isTransactionsEmpty = transactions?.length === 0;
+  const isTransactionsEmpty = data?.transactionsByDate?.length === 0;
 
   return (
-    <div className="">
+    <div className="flex flex-col">
       <div className="flex items-start gap-8">
         <div>
           <h1>Transações Geral</h1>
@@ -68,6 +70,15 @@ export default function Page() {
             <RegisterTransactionDialog type="EXPENSE" />
 
             <ExportTransactions />
+          </div>
+
+          <div className="mt-4 flex items-center gap-4">
+            <span className="text-green-500">
+              {transformToCurrency(data?.resume?.totalIncome ?? 0)}
+            </span>
+            <span className="text-red-500">
+              {transformToCurrency(data?.resume?.totalExpense ?? 0)}
+            </span>
           </div>
         </div>
 
@@ -153,86 +164,102 @@ export default function Page() {
         </div>
       )}
 
-      <div className="w-fit h-[calc(100vh-230px)] overflow-scroll mt-4 pr-4 pb-10 border-r border-r-neutral-800">
-        {transactions?.map((transaction) => (
-          <div key={transaction.date} className="mt-8 flex flex-col gap-3">
-            <div>
-              <p className="text-sm font-light text-neutral-300">
-                {format(new Date(transaction.date + "T00:00"), "dd 'de' MMMM", {
-                  locale: ptBR,
-                })}
-              </p>
+      <div className="flex gap-4">
+        <div className="w-fit h-[calc(100vh-230px)] overflow-scroll mt-4 pr-4 pb-10 border-r border-r-neutral-800">
+          {data?.transactionsByDate?.map((transaction) => (
+            <div key={transaction.date} className="mt-8 flex flex-col gap-3">
+              <div>
+                <p className="text-sm font-light text-neutral-300">
+                  {format(
+                    new Date(transaction.date + "T00:00"),
+                    "dd 'de' MMMM",
+                    {
+                      locale: ptBR,
+                    }
+                  )}
+                </p>
 
-              <div className="border-l border-l-neutral-700 ml-2 pl-4 pt-2 mt-2 flex flex-col gap-3">
-                {transaction.transactions.map((item) => {
-                  const isNotDescription =
-                    item.description?.length === 0 || !item.description;
+                <div className="border-l border-l-neutral-700 ml-2 pl-4 pt-2 mt-2 flex flex-col gap-3">
+                  {transaction.transactions.map((item) => {
+                    const isNotDescription =
+                      item.description?.length === 0 || !item.description;
 
-                  return (
-                    <div
-                      key={item.id}
-                      className="flex w-fit items-center gap-4 text-xs text-neutral-400"
-                    >
-                      <ImageBank bank={item.bank.bank} width={20} height={20} />
-                      <ImageCategory
-                        color={item.category.color}
-                        icon={item.category.icon as string}
-                        classNameBackground="size-5"
-                        classNameIcon="size-3"
-                      />
-                      <span
-                        className={cn("w-[200px]", {
-                          "text-neutral-700 text-center": isNotDescription,
-                        })}
+                    return (
+                      <div
+                        key={item.id}
+                        className="flex w-fit items-center gap-4 text-xs text-neutral-400"
                       >
-                        {isNotDescription ? "sem descrição" : item.description}
-                      </span>
-                      <span
-                        className={cn("w-12", {
-                          "text-red-400": item.type === "EXPENSE",
-                          "text-green-400": item.type === "INCOME",
-                        })}
-                      >
-                        {item.type === "INCOME" ? "Entrada" : "Saída"}
-                      </span>
-                      <span
-                        className={cn("text-base w-[100px]", {
-                          "text-red-400": item.type === "EXPENSE",
-                          "text-green-400": item.type === "INCOME",
-                        })}
-                      >
-                        {transformToCurrency(item.value)}
-                      </span>
-
-                      <div className="flex gap-2">
-                        <RegisterTransactionDialog
-                          transactionId={item.id}
-                          type={item.type}
-                          actions="EDIT"
-                          bankId={item.bank.id}
-                          category={item.category.id}
-                          description={item.description ?? ""}
-                          value={transformToCurrency(item.value)}
-                          datetime={item.date}
+                        <ImageBank
+                          bank={item.bank.bank}
+                          width={20}
+                          height={20}
                         />
-                        <RegisterTransactionDialog
-                          type={item.type}
-                          actions="COPY"
-                          bankId={item.bank.id}
-                          category={item.category.id}
-                          description={item.description ?? ""}
-                          value={transformToCurrency(item.value)}
-                          datetime={item.date}
+                        <ImageCategory
+                          color={item.category.color}
+                          icon={item.category.icon as string}
+                          classNameBackground="size-5"
+                          classNameIcon="size-3"
                         />
-                        <DialogDeleteTransaction id={item.id} />
+                        <span
+                          className={cn("w-[200px]", {
+                            "text-neutral-700 text-center": isNotDescription,
+                          })}
+                        >
+                          {isNotDescription
+                            ? "sem descrição"
+                            : item.description}
+                        </span>
+                        <span
+                          className={cn("w-12", {
+                            "text-red-400": item.type === "EXPENSE",
+                            "text-green-400": item.type === "INCOME",
+                          })}
+                        >
+                          {item.type === "INCOME" ? "Entrada" : "Saída"}
+                        </span>
+                        <span
+                          className={cn("text-base w-[100px]", {
+                            "text-red-400": item.type === "EXPENSE",
+                            "text-green-400": item.type === "INCOME",
+                          })}
+                        >
+                          {transformToCurrency(item.value)}
+                        </span>
+
+                        <div className="flex gap-2">
+                          <RegisterTransactionDialog
+                            transactionId={item.id}
+                            type={item.type}
+                            actions="EDIT"
+                            bankId={item.bank.id}
+                            category={item.category.id}
+                            description={item.description ?? ""}
+                            value={transformToCurrency(item.value)}
+                            datetime={item.date}
+                          />
+                          <RegisterTransactionDialog
+                            type={item.type}
+                            actions="COPY"
+                            bankId={item.bank.id}
+                            category={item.category.id}
+                            description={item.description ?? ""}
+                            value={transformToCurrency(item.value)}
+                            datetime={item.date}
+                          />
+                          <DialogDeleteTransaction id={item.id} />
+                        </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="flex-1 flex justify-center">
+          <RechartIncome />
+          <RechartExpense />
+        </div>
       </div>
     </div>
   );
